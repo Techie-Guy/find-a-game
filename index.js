88eec4d518eb52b8c70503f169beb840a8e39dc7
Techie-Guy <insleeping13@gmail.com>
2022-07-17 16:50:14 +0530

    Added CORS

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 8080;
+
+app.use(cors({
+    origin: '*'
+}));
 
 app.set('json spaces', 2);
 
